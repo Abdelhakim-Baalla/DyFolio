027cec5f8a288bf4a1f4c15ab16b943a546a2577
Abdelhakim Baalla <abdelhakimbaalla50@gmail.com>
2025-10-31 19:52:56 +0100

    Ajout du modèle Competence avec schéma et interface pour la gestion des compétences

--- src/models/index.ts
+++ src/models/index.ts
@@ -2,15 +2,18 @@ const userMod = require('./Utilisateur');
 const profilMod = require('./Profil');
 const experienceMod = require('./Experience');
 const categorieMod = require('./Categorie');
+const competenceMod = require('./Competence');
 
 const Utilisateur = userMod && userMod.default ? userMod.default : userMod;
 const Profil = profilMod && profilMod.default ? profilMod.default : profilMod;
 const Experience = experienceMod && experienceMod.default ? experienceMod.default : experienceMod;
 const Categorie = categorieMod && categorieMod.default ? categorieMod.default : categorieMod;
+const Competence = competenceMod && competenceMod.default ? competenceMod.default : competenceMod;
 
 module.exports = {
   Utilisateur,
   Profil,
   Experience,
   Categorie,
+  Competence,
 };
