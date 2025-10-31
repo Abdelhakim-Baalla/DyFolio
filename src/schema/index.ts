b739a01cef7eb760d4f7d9d895aa82624b61937e
Abdelhakim Baalla <abdelhakimbaalla50@gmail.com>
2025-10-31 21:47:54 +0100

    Ajout des types Projet, Competence, Experience, Portfolio et Profil dans le schéma GraphQL

--- src/schema/index.ts
+++ src/schema/index.ts
@@ -2,7 +2,46 @@ import { gql } from 'graphql-tag';
 
 
 export const typeDefs = gql`
+  type Projet {
+    titre: String!
+    description: String
+    image: String
+    lienDemo: String
+    lienCode: String
+  }
+
+  type Competence {
+    nom: String!
+    niveau: Int!
+  }
+
+  type Experience {
+    poste: String!
+    entreprise: String!
+    description: String
+    dateDebut: String
+    dateFin: String
+  }
+
+  type Portfolio {
+    bio: String
+    projets: [Projet!]!
+    competences: [Competence!]!
+    experiences: [Experience!]!
+  }
+
+  type Profil {
+    nom: String!
+    prenom: String!
+    metier: String!
+    bio: String
+  }
+
   type Query {
-    hello: String!
+    getPortfolio: Portfolio!
+    getProfil: Profil!
+    getProjets: [Projet!]!
+    getCompetences: [Competence!]!
+    getExperiences: [Experience!]!
   }
 `;
