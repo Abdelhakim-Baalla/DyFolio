4bba6e177d2f6ea8ab2b98ec74e59052b0a38376
Abdelhakim Baalla <abdelhakimbaalla50@gmail.com>
2025-11-05 00:11:07 +0100

    feat: réorganiser la configuration du serveur dans vite.config.js

--- DyFolio-Front/vite.config.js
+++ DyFolio-Front/vite.config.js
@@ -1,13 +1,24 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react({
       babel: {
-        plugins: [['babel-plugin-react-compiler']],
+        plugins: [["babel-plugin-react-compiler"]],
       },
     }),
   ],
-})
+  server: {
+    port: 3000,
+    watch: {
+      usePolling: true,
+      interval: 100,
+    },
+    hmr: {
+      overlay: true,
+      protocol: "ws",
+    },
+  },
+});
