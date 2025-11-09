133a489ece65248b94a59294c49008dd7b6d9d96
Abdelhakim Baalla <abdelhakimbaalla50@gmail.com>
2025-11-09 17:42:54 +0100

    Suppression des composants ScrollToTop et Breadcrumb de l'application pour simplifier la structure.

--- DyFolio-Front/src/App.jsx
+++ DyFolio-Front/src/App.jsx
@@ -1,15 +1,11 @@
 import { Outlet } from 'react-router-dom';
 import Navigation from './components/Navigation';
 import Footer from './components/Footer';
-import ScrollToTop from './components/ScrollToTop';
-import Breadcrumb from './components/Breadcrumb';
 
 export default function App() {
   return (
     <div className="min-h-screen bg-gray-50 flex flex-col">
-      <ScrollToTop />
       <Navigation />
-      <Breadcrumb />
       <main className="flex-grow">
         <Outlet />
       </main>
