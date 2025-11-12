f24e46a225c55b5a35b0265f4c016dae5f1fe8b6
Abdelhakim Baalla <abdelhakimbaalla50@gmail.com>
2025-11-12 15:13:11 +0100

    Amélioration de la mise en page du composant Home avec des sections révisées, ajout de statistiques et de services, et optimisation du composant App pour une meilleure expérience utilisateur.

--- DyFolio-Front/src/App.jsx
+++ DyFolio-Front/src/App.jsx
@@ -6,7 +6,7 @@ export default function App() {
   return (
     <div className="min-h-screen bg-gray-50 flex flex-col">
       <Navigation />
-      <main className="flex-grow">
+      <main className="flex-grow w-full">
         <Outlet />
       </main>
       <Footer />
